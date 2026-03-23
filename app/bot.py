c94b42b32e53b070922fc7203e3db43498f34193
Kots <220512286+thalorein@users.noreply.github.com>
2026-03-23 22:28:00 +0800

    Error handling for guilds

--- app/bot.py
+++ app/bot.py
@@ -2,6 +2,7 @@ import logging
 from typing import List
 
 import discord
+from discord.errors import Forbidden
 from discord.ext import commands
 
 from .db import Db
@@ -44,7 +45,12 @@ class FireStormBot(commands.Bot):
         )
         # Sync commands to guild immediately (if not it will take an hour)
         for guild in self._guilds:
-            await self.tree.sync(guild=guild)
+            try:
+                await self.tree.sync(guild=guild)
+            except Forbidden:
+                logger.warning(f"[Forbidden] Skipping sync to guild {guild}")
+            except Exception as e: # pylint: disable=broad-exception-caught
+                logger.warning(f"[Unknown Error] Skipping sync to guild {guild}: {e}")
         logger.info("App commands loaded and synced!")
 
     async def on_ready(self):
